26a95cd20288a8b1a32244d7513c65dfb0969e02
arronno1234 <69010903+arronno1234@users.noreply.github.com>
2020-08-05 00:24:45 +0600

    calculator

---
+++ script.js
@@ -0,0 +1,82 @@
+function seven(){
+var seven=parseInt(document.getElementsByTagName("button")[0].value);
+ document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+seven;
+}
+
+function eight(){
+    var eight=parseInt(document.getElementsByTagName("button")[1].value);
+    document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+eight;
+    }
+
+    function nine(){
+        var nine=parseInt(document.getElementsByTagName("button")[2].value);
+        document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+nine;
+        }
+
+        function de(){
+            var de=(document.getElementsByTagName("button")[3].value);
+            document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+de;
+            }
+            
+ function four(){
+         var four=parseInt(document.getElementsByTagName("button")[4].value);
+        document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+four;
+                }
+                function five(){
+                    var five=parseInt(document.getElementsByTagName("button")[5].value);
+                    document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+five;
+                    }
+
+function six(){
+var six=parseInt(document.getElementsByTagName("button")[6].value);
+document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+six;
+}
+
+
+function into(){
+    var into=(document.getElementsByTagName("button")[7].value);
+    document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+into;
+    }
+
+
+    function one(){
+        var one=parseInt(document.getElementsByTagName("button")[8].value);
+        document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+one;
+        }
+
+function two(){
+            var two=parseInt(document.getElementsByTagName("button")[9].value);
+            document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+two;
+}
+
+
+function three(){
+    var three=parseInt(document.getElementsByTagName("button")[10].value);
+    document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+three;
+    }
+
+
+function min(){
+        var min=(document.getElementsByTagName("button")[11].value);
+        document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+min;
+        }
+
+
+        function zero(){
+            var zero=parseInt(document.getElementsByTagName("button")[12].value);
+            document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+zero;
+            }
+    
+function c(){
+    document.getElementsByClassName("display")[0].value=null;
+}
+
+function ans(){
+    document.getElementsByClassName("display")[0].value=eval(document.getElementsByClassName("display")[0].value);
+}
+
+
+function plus(){
+    var plus=(document.getElementsByTagName("button")[15].value);
+    document.getElementsByClassName("display")[0].value=document.getElementsByClassName("display")[0].value+plus;
+    }
